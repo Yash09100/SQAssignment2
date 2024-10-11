afe6357b000e9dd91917c6df1817236e64ff52d3
Yash Khatri <yashkhatri906@gmail.com>
2024-10-11 19:21:44 -0400

    Add Nunit Project and write 1 Test Case

---
+++ TriangleUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TriangleUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TriangleUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0b8b4893-fab4-4713-8383-8a77f6b908c8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TriangleUnitTests/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using TriangleSolver;
+using NUnit.Framework;
+
+namespace TriangleUnitTests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void Tringle_InputSameSides_OutputEquilateral()
+        {
+            // Arrange
+            int a = 3;
+            int b = 3;
+            int c = 3;
+
+            // Act
+            string output = Triangle.AnalyzeTriangle(a, b, c);
+
+            // Assert
+            Assert.That("Equilateral triangle", Is.EqualTo(output));
+        }
+    }
+}
